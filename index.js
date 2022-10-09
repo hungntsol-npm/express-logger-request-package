767d99476aafbe7ae574b860d4ad0f274841984c
Hung Nguyen <hungntsol77@gmail.com>
2022-10-09 17:45:36 +0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const loggerRequest = (req, res, next) => {
+	const ip = req.headers["x-forward-for"] || req.socket.remoteAddress;
+	const route = req.originalUrl;
+	const method = req.method;
+
+	next();
+
+	const statusCode = res ? res.statusCode.toString() : "";
+
+	console.log(
+		`${ip} -- [${new Date().toGMTString()}] "${method} ${route} ${statusCode}"`
+	);
+};
+
+export default loggerRequest;
